b354509219322232a75081254b601b40fcbddfe8
Gabriel Larez <gabrielnoriega@gmail.com>
2024-03-27 20:09:04 -0300

    puertos

--- back/server.js
+++ back/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import { logger } from "logger-express";
 
+
 import loginRoutes from "./config/routes/loginRoutes.js";
 import userRoutes from './config/routes/userRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
--- front/src/config/constans.js
+++ front/src/config/constans.js
@@ -1,4 +1,4 @@
-export const URLBASE = 'http://localhost:5173'
+export const URLBASE = 'http://localhost:3000'
 
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
